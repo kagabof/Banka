7c8a521f6fe2b72bc9e3cc0d78a0380e2fa0290a
kagabo <faustinkagabo@gmail.com>
2019-04-04 15:40:11 +0200

    [ft 165121296] Add account db

---
+++ server/db/accountDB.js
@@ -0,0 +1,12 @@
+
+const accounts = [{
+    id: 1,
+    accountNumber: 123456,
+    createOn: Date.now(),
+    owner: 1,
+    type: "current",
+    status: "active",
+    balance: 0.0,
+}];
+
+export default accounts;
