9cfb93568cee9fcf4a67dc1d2d20512b115ac993
kagabo <faustinkagabo@gmail.com>
2019-04-06 16:47:12 +0200

    chang in testing account

---
+++ server/tests/testAccount.js
@@ -0,0 +1,15 @@
+import chai from "chai";
+import chaiHttp from "chaiHttp";
+import userDb from "./../db/userDB";
+import accountDb from "./../db/accountDB";
+import app from "./../app";
+
+chai.use(chaiHttp);
+chai.should();
+
+describe("Account", ()=>{
+    describe("POST /", ()=>{
+        
+    })
+})
+
